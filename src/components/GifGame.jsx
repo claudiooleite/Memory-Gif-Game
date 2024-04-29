5e6248f86a529d63cd54cc07aac5168eb68ef996
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-04-29 12:04:29 +0200

    cards styled

--- src/components/GifGame.jsx
+++ src/components/GifGame.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { Card, CardHeader, CardBody, CardFooter, Image, SimpleGrid } from '@chakra-ui/react'
+
 
 function GifViewer() {
   const [gif, setGif] = useState([]);
@@ -10,9 +12,10 @@ function GifViewer() {
   }, []);
 
   const fetchGif = async () => {
+    console.log('Fetching GIFs...')
     try {
       if (fetchedGifsRef.current.length === 0) {
-        const apiKey = 'uQB4hZ2aP7ZEBsexGPdnVkRHk08iBHgk';
+        const apiKey = 'WF78jLTK7KdHUFF3D3lMToYck7xWqWYR';
         const fetchedGifs = [];
         for (let i = 0; i < numGifs; i++) {
           const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=&rating=g`);
@@ -24,6 +27,7 @@ function GifViewer() {
       } else {
         setGif(fetchedGifsRef.current);
       }
+      console.log('GIFs fetched successfully');
     } catch (error) {
       console.error('Error fetching gifs:', error);
     }
@@ -44,28 +48,38 @@ function GifViewer() {
     setGif(fetchedGifsRef.current.slice(0, newNumGifs)); // Update displayed GIFs based on new number
   };  
 
+ 
+
   return (
     <>
-      <div>
-        <div className="gif-container">
-          {numGifs > 0 && gif.map((gif, id) => (
-            <div className='gif-image' key={gif.id}>
-              <img src={gif.images.original.url} alt={`GIF ${id + 1}`} />
-            </div>
-          ))}
-        </div>
-        <div className="controls">
-          <label htmlFor="numGifs">Number of GIFs:</label>
-          <select id="numGifs" value={numGifs} onChange={handleNumGifsChange}>
-            <option value="3">3 GIFs</option>
-            <option value="6">6 GIFs</option>
-            <option value="9">9 GIFs</option>
-          </select>
-          <button onClick={shuffleImages}>Shuffle GIFs</button>
-        </div>
+      <SimpleGrid  spacing='40px'  minChildWidth='120px'>  
+            {numGifs > 0 && gif.map((gif, id) => (
+              <Card key={gif.id} height={gif.images.original.fixed_height}>
+                <Image 
+                    src={gif.images.original.url} 
+                    alt={`GIF ${id + 1}`} 
+                    borderRadius='lg'
+                    width="100%"
+                    height="100%"
+                    objectFit="cover"
+                    onClick={shuffleImages}
+                    cursor="pointer"
+                  />    
+                </Card>
+            ))}
+      </SimpleGrid>  
+
+      <div className="controls">
+        <label htmlFor="numGifs">Number of GIFs:</label>
+        <select id="numGifs" value={numGifs} onChange={handleNumGifsChange}>
+          <option value="3">3 GIFs</option>
+          <option value="6">6 GIFs</option>
+          <option value="9">9 GIFs</option>
+        </select>
+        <button onClick={shuffleImages}>Shuffle GIFs</button>
       </div>
     </>
-  );
+    );
 }
 
 export default GifViewer;
